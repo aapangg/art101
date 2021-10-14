dab84fb721db66667b73b86accdbadcfff15276d
aapangg <91556495+aapangg@users.noreply.github.com>
2021-10-13 19:35:21 -0700

    final lab 4 code
    final

---
+++ lab4/js/lab.js
@@ -0,0 +1,19 @@
+<pre>
+<code>
+
+// This is a checkers game.
+// The point of this checkers game is to win lol.
+// @link   URL
+// @file   This files defines the ART101-01 class.
+// @author AnitaPang.
+// @since  10.13.2021
+
+
+// Display board before game starts
+// Evaluate user’s move after each turn
+// Evaluate possible moves
+// Choose best computer move
+// Repeat until winner is determined
+
+</code>
+</pre>
