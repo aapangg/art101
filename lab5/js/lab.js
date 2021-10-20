75483513ea8d0301e51c7738d52e9819582d20f4
aapangg <91556495+aapangg@users.noreply.github.com>
2021-10-20 15:30:39 -0700

    lab 6 final
    updated

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * AUTHOR: ANITA PANG <APANG6@UCSC.EDU>
+ * CREATED: 18 OCTOBER
+ * LICENSE: PUBLIC DOMAIN
+ */
+
+ // DEFINE VARIABLES
+ make = "Toyota";
+ model = "Highlander";
+ color = "White";
+ year = 2020;
+
+ // CALCULATE
+ age = 2020 - year;
+
+ // OUTPUT
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "years<br>");
