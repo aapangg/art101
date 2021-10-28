2d99fd7b6959fb00a03ddf7bb57c1f2bbaa75384
aapangg <91556495+aapangg@users.noreply.github.com>
2021-10-27 21:07:57 -0700

    lab 8 final

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,23 +1,23 @@
 /*
  * AUTHOR: ANITA PANG <APANG6@UCSC.EDU>
- * CREATED: 25 OCTOBER
+ * CREATED: 27 OCTOBER
  * LICENSE: PUBLIC DOMAIN
  */
 
-function sortUserName(){
-  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
-  console.log("userName =", userName);
-
-  var nameArray = userName.split('');
-  console.log("nameArray=", nameArray);
-
-  var nameArraySort = nameArray.sort();
-  console.log("nameArraySort =", nameArraySort);
-
-  var nameSorted = nameArraySort.join('');
-  console.log("nameSorted =", nameSorted);
-  return nameSorted;
+function isEven(x){
+    return (x%2 == 0);
 }
 
-document.writeln("Oh hey, I've fixed your name: ",
-    sortUserName(), "</br>");
+console.log('Is 1 even? ', isEven(1));
+console.log('Is 2 even? ', isEven(2));
+
+array = [100,81,4,16,42,10000];
+console.log('My array', array);
+
+var result = array.map(isEven);
+console.log('Test of evenness of array:', result);
+
+var result = array.map(function(x){
+    return x ** 0.5;
+})
+console.log('Squareroot of array: ',result);
